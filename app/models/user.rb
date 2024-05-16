857dd95f7657b190cc478dd543696e8e0c703da4
renta0525 <renzu525@gmail.com>
2024-05-16 16:58:25 +0900

    take1

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :prototypes
+  has_many :comments, dependent: :destroy
+
   validates :name, presence: true
   validates :profile, presence: true
   validates :occupation, presence: true
   validates :position, presence: true
 
-  has_many :prototypes
-  has_many :comments, dependent: :destroy
   
 end
