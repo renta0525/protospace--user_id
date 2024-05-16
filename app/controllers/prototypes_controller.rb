0b7e12be1fd687c482251e6b7d4b961dfbe707ad
renta0525 <renzu525@gmail.com>
2024-05-17 01:15:43 +0900

    take3

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -2,6 +2,7 @@ class PrototypesController < ApplicationController
   before_action :authenticate_user!, only:[:new, :edit, :destroy]
   before_action :move_to_index_logout, except: [:index, :show]
   before_action :move_to_index_login, only: [:edit]
+  
   def index
     @prototypes = Prototype.all
   end
